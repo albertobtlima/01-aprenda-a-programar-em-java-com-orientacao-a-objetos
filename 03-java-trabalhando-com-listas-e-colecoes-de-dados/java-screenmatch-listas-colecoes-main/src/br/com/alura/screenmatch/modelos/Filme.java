e17d23d0decb528c8ca41432795fdd67d4448965
Alberto Lima <alberto.btlima@outlook.com>
2024-10-21 20:28:01 -0300

    add: construtor da clase Filme

--- 03-java-trabalhando-com-listas-e-colecoes-de-dados/java-screenmatch-listas-colecoes-main/src/br/com/alura/screenmatch/modelos/Filme.java
+++ 03-java-trabalhando-com-listas-e-colecoes-de-dados/java-screenmatch-listas-colecoes-main/src/br/com/alura/screenmatch/modelos/Filme.java
@@ -3,18 +3,27 @@ package br.com.alura.screenmatch.modelos;
 import br.com.alura.screenmatch.calculos.Classificavel;
 
 public class Filme extends Titulo implements Classificavel {
-    private String diretor;
+  private String diretor;
 
-    public String getDiretor() {
-        return diretor;
-    }
+  public Filme(String nome) {
+    this.setNome(nome);
+  }
 
-    public void setDiretor(String diretor) {
-        this.diretor = diretor;
-    }
+  public String getDiretor() {
+    return diretor;
+  }
 
-    @Override
-    public int getClassificacao() {
-        return (int) pegaMedia() / 2;
-    }
+  public void setDiretor(String diretor) {
+    this.diretor = diretor;
+  }
+
+  @Override
+  public int getClassificacao() {
+    return (int) pegaMedia() / 2;
+  }
+
+  @Override
+  public String toString() {
+    return "Filme: " + this.getNome() + " (" + this.getAnoDeLancamento() + ")";
+  }
 }
