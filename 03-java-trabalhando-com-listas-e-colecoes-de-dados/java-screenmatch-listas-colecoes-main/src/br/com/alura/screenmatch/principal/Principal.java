6c957b15bb6bcec60272aa61fd5806d1a2059d81
Alberto Lima <alberto.btlima@outlook.com>
2024-10-21 21:31:14 -0300

    add: principal com listas

--- 03-java-trabalhando-com-listas-e-colecoes-de-dados/java-screenmatch-listas-colecoes-main/src/br/com/alura/screenmatch/principal/Principal.java
+++ 03-java-trabalhando-com-listas-e-colecoes-de-dados/java-screenmatch-listas-colecoes-main/src/br/com/alura/screenmatch/principal/Principal.java
@@ -1,3 +1,5 @@
+package br.com.alura.screenmatch.principal;
+
 import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
 import br.com.alura.screenmatch.calculos.FiltroRecomendacao;
 import br.com.alura.screenmatch.modelos.Episodio;
@@ -8,8 +10,7 @@ import java.util.ArrayList;
 
 public class Principal {
   public static void main(String[] args) {
-    Filme meuFilme = new Filme("O poderoso chefão");
-    meuFilme.setAnoDeLancamento(1970);
+    Filme meuFilme = new Filme("O poderoso chefão", 1970);
     meuFilme.setDuracaoEmMinutos(180);
     System.out.println("Duração do filme: " + meuFilme.getDuracaoEmMinutos());
 
@@ -19,21 +20,15 @@ public class Principal {
     meuFilme.avalia(10);
     System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
     System.out.println(meuFilme.pegaMedia());
-    //meuFilme.somaDasAvaliacoes = 10;
-    //meuFilme.totalDeAvaliacoes = 1;
-    //System.out.println(meuFilme.pegaMedia());
 
-    Serie lost = new Serie();
-    lost.setNome("Lost");
-    lost.setAnoDeLancamento(2000);
+    Serie lost = new Serie("Lost", 2000);
     lost.exibeFichaTecnica();
     lost.setTemporadas(10);
     lost.setEpisodiosPorTemporada(10);
     lost.setMinutosPorEpisodio(50);
     System.out.println("Duração para maratonar Lost: " + lost.getDuracaoEmMinutos());
 
-    Filme outroFilme = new Filme("Avatar");
-    outroFilme.setAnoDeLancamento(2023);
+    Filme outroFilme = new Filme("Avatar", 2023);
     outroFilme.setDuracaoEmMinutos(200);
 
     CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
@@ -51,9 +46,8 @@ public class Principal {
     episodio.setTotalVisualizacoes(300);
     filtro.filtra(episodio);
 
-    var filmeDoGato = new Filme("Dogville");
+    var filmeDoGato = new Filme("Dogville", 2003);
     filmeDoGato.setDuracaoEmMinutos(200);
-    filmeDoGato.setAnoDeLancamento(2003);
     filmeDoGato.avalia(9);
 
     ArrayList<Filme> listaDeFilmes = new ArrayList<>();
