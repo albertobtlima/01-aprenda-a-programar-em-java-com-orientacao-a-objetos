9d311d5f0bab62e78520660382dd8cda7572c507
Alberto Lima <131916622+albertobtlima@users.noreply.github.com>
2024-10-22 12:20:12 -0300

    update PrincipalComListas

--- 03-java-trabalhando-com-listas-e-colecoes-de-dados/java-screenmatch-listas-colecoes-main/src/br/com/alura/screenmatch/principal/PrincipalComListas.java
+++ 03-java-trabalhando-com-listas-e-colecoes-de-dados/java-screenmatch-listas-colecoes-main/src/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -4,9 +4,7 @@ import br.com.alura.screenmatch.modelos.Filme;
 import br.com.alura.screenmatch.modelos.Serie;
 import br.com.alura.screenmatch.modelos.Titulo;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
+import java.util.*;
 
 public class PrincipalComListas {
   public static void main(String[] args) {
@@ -18,7 +16,7 @@ public class PrincipalComListas {
     meuFilme.avalia(7);
     Serie lost = new Serie("Lost", 2000);
 
-    ArrayList<Titulo> lista = new ArrayList<>();
+    List<Titulo> lista = new LinkedList<>();
     lista.add(filmeDoGato);
     lista.add(meuFilme);
     lista.add(outroFilme);
