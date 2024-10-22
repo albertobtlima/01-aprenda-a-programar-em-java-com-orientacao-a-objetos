b95bfad9e39b8d5e2a6b34399266fa692dd4c8f9
Alberto Lima <131916622+albertobtlima@users.noreply.github.com>
2024-10-22 13:18:13 -0300

    nova aula

---
+++ 03-java-trabalhando-com-listas-e-colecoes-de-dados/02-comprars/src/Compra.java
@@ -0,0 +1,22 @@
+public class Compra {
+  private String produto;
+  private double valor;
+
+  public Compra(String produto, double valor) {
+    this.produto = produto;
+    this.valor = valor;
+  }
+
+  public String getProduto() {
+    return produto;
+  }
+
+  public double getValor() {
+    return valor;
+  }
+
+  @Override
+  public String toString() {
+    return "Compra: produto = " + produto + ", valor = " + valor;
+  }
+}
