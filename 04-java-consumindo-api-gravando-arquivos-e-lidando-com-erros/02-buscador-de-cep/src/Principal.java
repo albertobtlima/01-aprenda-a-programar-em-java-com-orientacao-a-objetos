e73b9d4fc76be462ccd536bb113aeafceee7449d
Alberto Lima <alberto.btlima@outlook.com>
2024-11-18 16:44:26 -0300

    add: GeradorDeArquivo

--- 04-java-consumindo-api-gravando-arquivos-e-lidando-com-erros/02-buscador-de-cep/src/Principal.java
+++ 04-java-consumindo-api-gravando-arquivos-e-lidando-com-erros/02-buscador-de-cep/src/Principal.java
@@ -1,8 +1,22 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Principal {
   public static void main(String[] args) {
+    Scanner leitura = new Scanner(System.in);
     ConsultaCep consultaCep = new ConsultaCep();
-    Endereco novoEndereco = consultaCep.buscaEndereco("01001000");
 
-    System.out.println(novoEndereco);
+    System.out.println("Digite o número do CEP para pesquisa: ");
+    var cep = leitura.nextLine();
+
+    try {
+      Endereco novoEndereco = consultaCep.buscaEndereco(cep);
+      System.out.println(novoEndereco);
+      GeradorDeArquivo geradorDeArquivo = new GeradorDeArquivo();
+      geradorDeArquivo.salvaJson(novoEndereco);
+    } catch (RuntimeException | IOException e) {
+      System.out.println(e.getMessage());
+      System.out.println("Finalizando aplicação...");
+    }
   }
 }
